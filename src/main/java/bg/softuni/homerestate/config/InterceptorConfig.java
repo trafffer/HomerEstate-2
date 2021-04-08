d853cdef94066f7b48b2ac67928e7e5e4362af9f
trafffer <sonya0@mail.bg>
2021-04-08 19:14:20 +0300

    Added 2 interceptors in the code.

--- src/main/java/bg/softuni/homerestate/config/InterceptorConfig.java
+++ src/main/java/bg/softuni/homerestate/config/InterceptorConfig.java
@@ -1,5 +1,7 @@
 package bg.softuni.homerestate.config;
 
+import bg.softuni.homerestate.web.interceptors.TracePostRequestInterceptor;
+import bg.softuni.homerestate.web.interceptors.TraceResponseInterceptor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -7,11 +9,18 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class InterceptorConfig implements WebMvcConfigurer {
 
+    private final TracePostRequestInterceptor tracePostRequestInterceptor;
+    private final TraceResponseInterceptor traceResponseInterceptor;
 
-
+    public InterceptorConfig(TracePostRequestInterceptor tracePostRequestInterceptor,
+                             TraceResponseInterceptor traceResponseInterceptor) {
+        this.tracePostRequestInterceptor = tracePostRequestInterceptor;
+        this.traceResponseInterceptor = traceResponseInterceptor;
+    }
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
-
+         registry.addInterceptor(tracePostRequestInterceptor);
+         registry.addInterceptor(traceResponseInterceptor);
     }
 }
