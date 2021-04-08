2533fd241a48899cae0509b8188429ea7a651c3f
trafffer <sonya0@mail.bg>
2021-04-08 17:34:39 +0300

    Fixed a few bugs about the comment commit.

---
+++ src/main/java/bg/softuni/homerestate/config/InterceptorConfig.java
@@ -0,0 +1,17 @@
+package bg.softuni.homerestate.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class InterceptorConfig implements WebMvcConfigurer {
+
+
+
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+
+    }
+}
