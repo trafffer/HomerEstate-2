8fd032b7dcee996341ef0493bd8896431b4422c5
trafffer <sonya0@mail.bg>
2021-05-21 21:24:46 +0300

    Some more changes to the carousel on page details-offer.html. Fixed issue with bad sliding. Fixed the css about carousel.

--- src/main/resources/static/js/newCarousel.js
+++ src/main/resources/static/js/newCarousel.js
@@ -5,7 +5,7 @@ const dots = Array.from(indicator.children);
 const slideWidth = slides[0].getBoundingClientRect().width;
 
 const setSlidePosition =  (slide,index)=> {
-    slide.style.width = screen.width.valueOf()*0.30;
+    slide.style.width = screen.availWidth*0.3 + 'px';
     slide.style.left = slideWidth * index + 'px';
 };
 
@@ -14,14 +14,11 @@ slides.forEach(setSlidePosition);
 let a = 0;
 if (a===0) {
     slides[0].className += ' active';
+    dots[0].className += ' current-dot';
     a++;
 }
 
-const currentSlide = track.querySelector('.active')
-const nextSlide = currentSlide.nextElementSibling;
-nextSlide.className += ' next';
 
-const amountToMove = nextSlide.style.left;
 
 const moveToSlide = (track, currentSlide, targetSlide)=>{
     track.style.transform = 'translateX(-' +targetSlide.style.left;
@@ -29,13 +26,22 @@ const moveToSlide = (track, currentSlide, targetSlide)=>{
     targetSlide.classList.add('active');
 }
 
+const updateDots = (currentDot, targetDot) => {
+    currentDot.classList.remove('current-dot');
+    targetDot.classList.add('current-dot')
+}
 
-track.addEventListener('drag',e=>{
 
-
-
-
+indicator.addEventListener('click',e=>{
+  const targetDot = e.target.closest('button');
+  if (!targetDot)return ;
+  const currentSlide = track.querySelector('.active');
+  const currentDot = indicator.querySelector('.current-dot');
+  const targetIndex = dots.findIndex(dot => dot === targetDot);
+  const targetSlide = slides[targetIndex];
+  moveToSlide(track,currentSlide,targetSlide);
+  updateDots(currentDot,targetDot);
 })
 
-console.log(nextSlide);
+
 
